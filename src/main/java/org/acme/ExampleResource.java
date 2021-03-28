5510a77e78803098bc15538caab5d01c716dc84d
Robson Oliveira Da Costa <robsondeveloper@gmail.com>
2021-03-28 14:53:57 -0400

    pega valor de variavel de ambiente

--- src/main/java/org/acme/ExampleResource.java
+++ src/main/java/org/acme/ExampleResource.java
@@ -5,6 +5,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 
 @Path("/hello")
@@ -12,10 +13,14 @@ public class ExampleResource {
 
 	private static final Logger LOG = Logger.getLogger(ExampleResource.class);
 
+	@ConfigProperty(name = "USER") // Environment Variables
+	private String user;
+
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String hello() {
 		LOG.info("Hello");
+		LOG.info("usuario: " + user);
 		return "Hello RESTEasy";
 	}
 }
